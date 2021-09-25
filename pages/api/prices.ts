91fcce8399355c35228e3ce25c89cd429702e21c
Bryan Reed <brian.reed.software@outlook.com>
2021-09-25 16:46:30 -0700

    first commit

---
+++ pages/api/prices.ts
@@ -0,0 +1,44 @@
+async function get(req, res): Promise<any> {
+    return 'Helpful information'
+  }
+  async function external(url: string, config?: object): Promise<any> {
+    const res = await fetch(url, config)
+    const data = await res.json()
+    return data
+  }
+  async function post(req, res): Promise<any> {
+    const params = {
+      denominator: req.body.currency,
+      exchange: req.body.exchange,
+      asset: req.body.symbol,
+    };
+    const priceUrl = new URL('https://crypto-asset-market-data-unified-apis-for-professionals.p.rapidapi.com/api/v1/exchanges/trades');
+    priceUrl.search = (new URLSearchParams(params)).toString();
+    const remoteResponse = await external(
+      priceUrl.toString(),
+      {
+        headers: {
+          'x-rapidapi-host': 'brand-recognition.p.rapidapi.com',
+          'x-rapidapi-key': '1f9828dd2fmsh8acb22645d8732dp1bf4b6jsnea031b0959b4',
+        }
+      }
+    )
+    const response = Object.assign({
+      currency: req.body.currency,
+      exchange: req.body.exchange,
+      symbol: req.body.symbol,
+    }, remoteResponse)
+    return response
+  }
+  export default async function (req, res): Promise<any> {
+    switch (req.method) {
+      case 'GET':
+        res.status(200).send(await get(req, res))
+        break
+      case 'POST':
+        res.status(200).json(await post(req, res))
+        break
+      default:
+        res.status(405).end() //Method Not Allowed
+    }
+  }
---
+++ pages/index.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import Head from 'next/head'
+export default function Home() {
+  const [prices, setPrices] = useState([]);
+  async function getPrices(e): Promise<void> {
+    e.preventDefault() // prevent page from submitting form
+    const result = await (await fetch('/api/hello')).text()
+    setPrices(prices.concat(result))
+  }
+  function getValue(name: string): string {
+    const elements = document.getElementsByTagName('form')[0].elements
+    const element = elements.namedItem(name) as HTMLInputElement
+    return element.value || element.dataset.default
+  }
+  async function fetcher(data: object) {
+    const res = await fetch('/api/prices', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    })
+    return await res.text()
+  }
+  async function getPrices(e): Promise<void> {
+    e.preventDefault()
+    const result = await fetcher({
+      currency: getValue('currency'),
+      exchange: getValue('exchange'),
+      symbol: getValue('symbol'),
+    })
+    setPrices(prices.concat(result))
+  }
+  return (
+    <div className="container">
+      <Head>
+        <title>Crypto Prices</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h3 className="title"> Crypto Prices </h3>
+        <div className="grid">
+          <div className="form card">
+            <form lang="en">
+              <div>
+                <div>
+                  <label htmlFor="symbol">Symbol:</label>
+                  <input type="text" id="symbol" name="symbol" data-default="BTC" placeholder="BTC" />
+                </div>
+                <div>
+                  <label htmlFor="currency">Currency:</label>
+                  <input type="text" id="currency" name="currency" data-default="USD" placeholder="USD" />
+                </div>
+                <div>
+                  <label htmlFor="exchange">Exchange:</label>
+                  <input type="text" id="exchange" name="exchange" data-default="Kraken" placeholder="Kraken" />
+                </div>
+              </div>
+              <div className="submit">
+                <button className="submit" onClick={getPrices}>Get Prices</button> &rarr;
+              </div>
+            </form>
+          </div>
+          <div className="code card">
+            <ul>
+              {prices.map((price, i) => <li key={i}><code>{price}</code></li>)}
+            </ul>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
